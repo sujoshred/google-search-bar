f7a88c36a6ecd3ddc78725b3175b72ef712da170
sujoshred <82267137+sujoshred@users.noreply.github.com>
2022-09-12 02:16:57 +0530

    first-commit

---
+++ main.js
@@ -0,0 +1,5 @@
+var searchBox = document.getElementById("searchBox");
+var googleIcon = document.getElementById("googleIcon");
+googleIcon.onclick = function() {
+  searchBox.classList.toggle("active");
+}
